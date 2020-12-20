f39060df420ffe61bf63093eb4e784e4be325609
Sean Twie03 <sean.twie03@gmail.com>
2020-12-20 08:26:59 -0600

    Initial commit. Spring Boot Web application with database connection.

---
+++ src/main/java/site/twiehaus/tazbbudget/TazbbudgetApplication.java
@@ -0,0 +1,13 @@
+package site.twiehaus.tazbbudget;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TazbbudgetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TazbbudgetApplication.class, args);
+    }
+
+}
---
+++ src/test/java/site/twiehaus/tazbbudget/TazbbudgetApplicationTests.java
@@ -0,0 +1,13 @@
+package site.twiehaus.tazbbudget;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TazbbudgetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
